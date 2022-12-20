e8515dd5ff83e3f8af659d68d6eb41a993db55a3
hcweigand10 <henryweigand10@gmail.com>
2022-12-19 17:14:08 -0800

    delete commments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const spotifyRoutes = require('./spotifyRoutes');
+const checkoutRoutes = require('./checkoutRoutes');
 
+router.use('/checkout', checkoutRoutes);
+router.use('/spotify', spotifyRoutes);
 router.use('/users', userRoutes);
 
 
--- server.js
+++ server.js
@@ -53,13 +53,8 @@ app.use((req, res, next) => {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 app.use(routes);
-
-
-// After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
-// Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
-
-
 
 db.connection
   .on("open", () => {
